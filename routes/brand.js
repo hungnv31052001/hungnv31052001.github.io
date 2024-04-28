677e06e877ca09628a668dcf313aea3277748ec1
hungnv31052001 <hungnv.31052001@gmail.com>
2024-04-28 19:09:30 +0700

    Deploy

--- routes/brand.js
+++ routes/brand.js
@@ -7,19 +7,38 @@ router.get('/', async (req, res) => {
    res.render('brand/index', { brandList });
 })
 
-// //step 1: render "Add toy" form for user to input data
-// router.get('/add', async (req, res) => {
-//    var brands = await BrandModel.find({});
-//    res.render('brand/add', { brands });
-// })
+//step 1: render "Add toy" form for user to input data
+router.get('/add', async (req, res) => {
+   var brands = await BrandModel.find({});
+   res.render('brand/add', { brands });
+})
 
-// //step 2: get input data from form and add data to database
-// router.post('/add', async (req, res) => {
-//    //get input data from form
-//    var brand = req.body;
-//    //add data to database
-//    await BrandModel.create(type);
-//    //redirect to toy homepage
-//    res.redirect('/brand');
-// })
+//step 2: get input data from form and add data to database
+router.post('/add', async (req, res) => {
+   //get input data from form
+   var brand = req.body;
+   //add data to database
+   await BrandModel.create(brand);
+   //redirect to toy homepage
+   res.redirect('/brand');
+})
+
+router.get('/delete/:id', async (req, res) => {
+   await BrandModel.findByIdAndDelete(req.params.id);
+   res.redirect('/brand');
+})
+
+router.get('/edit/:id', async (req, res) => {
+   var id = req.params.id;
+   var brand = await BrandModel.findById(id);
+   res.render('brand/edit', { brand });
+})
+
+router.post('/edit/:id', async (req, res) => {
+   var id = req.params.id;
+   var brand = req.body;
+   await BrandModel.findByIdAndUpdate(id, type);
+   res.redirect('/brand');
+})
+
 module.exports = router;
